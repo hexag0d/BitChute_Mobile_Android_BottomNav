d35befa6c55b26f3b261b053658812b9ae069ac6
hexagod <hexagod@protonmail.com>
2020-10-09 17:15:09 -0400

    add video encoder apk

--- Classes/AppState.cs
+++ Classes/AppState.cs
@@ -63,6 +63,6 @@
         /// this string is used in the app settings fragment to notify user 
         /// of version they're running
         /// </summary>
-        public static string AppVersion = "28.6.0.API8.OpenGL.VideoProcessing.PtOffsetRedux_";
+        public static string AppVersion = "28.6.3.1.API8.OpenGL.VideoProcessing.PtOffsetRedux";
     }
 }
